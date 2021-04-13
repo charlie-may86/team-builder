98d74185bef4e8d350feb956fca95a69d2409c72
Charles May <charlie@Charless-MacBook-Pro.local>
2021-04-13 18:53:09 -0500

    simple form created

--- team-builder/src/index.js
+++ team-builder/src/index.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-// import App from './App';
-import reportWebVitals from "./reportWebVitals";
+import App from './App';
+// import reportWebVitals from "./reportWebVitals";
 
 //first, build a simple form with meets requirements.
 const teamList = [
@@ -11,38 +11,84 @@ const teamList = [
   { name: "matt", email: "matt@jdm.com", role: "wheel barrow man" },
 ];
 
+//intial values for the form itself
+const intialValues = {
+  name: "",
+  email: "",
+  role: "",
+};
 
+function SimpleForm() {
+  const [team, setTeam] = useState(teamList);
+  const [formValues, setFormValues] = useState(intialValues);
 
-function SimpeForm() {
-  const [team, setTeam] = useState(teamList)
+  const change = (evt) => {
+    //change the value in a form
+    const { name, value } = evt.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const submit = (evt) => {
+    //subit the form inputs to state
+    evt.preventDefault();
+    const newTeamMember = {
+      name: formValues.name.trim(),
+      email: formValues.email.trim(),
+      role: formValues.role.trim(),
+    };
+    setTeam([...team, newTeamMember]);
+    setFormValues(intialValues);
+  };
 
   return (
-    <div className='container'>
+    <div className="container">
       <h1>Simple Form</h1>
 
       {team.map((teamMember, idx) => {
-      return (
-        <div key={idx}>
-          {teamMember.name} is the {teamMember.role} and can be reached at {teamMember.email}
-        </div>
-      )
-    })}
+        return (
+          <div key={idx}>
+            {teamMember.name} is the {teamMember.role} and can be reached at{" "}
+            {teamMember.email}
+          </div>
+        );
+      })}
+      <form onSubmit={submit}>
+        <input
+          type="text"
+          name="name"
+          value={formValues.name}
+          onChange={change}
+          placeholder="name"
+        />
+        <input
+          type="email"
+          name="email"
+          value={formValues.email}
+          onChange={change}
+          placeholder="email address"
+        />
+        <input
+          type="text"
+          name="role"
+          value={formValues.role}
+          onChange={change}
+          placeholder="role"
+        />
+        <button>Submit</button>
+      </form>
     </div>
-
   );
-
-  
 }
 
 ReactDOM.render(
-  <React.StrictMode>
-    <SimpeForm />
-    {/* <App /> */}
-  </React.StrictMode>,
+  <>
+    {/* <SimpleForm /> */}
+    <App />
+  </>,
   document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
